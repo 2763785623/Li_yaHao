d883bdc6268aedf4afdae53bbafbb61e1e9f1950
liyahao <2763785623@qq.com>
2020-03-19 12:04:45 +0800

    3-19

---
+++ 3-19/src/components/index.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <h2>进来了！！！</h2>
+        <el-button type='warning' round>这是组件的按钮</el-button>
+        <el-button type='danger' circle disabled>哦</el-button>
+    
+        <el-button type="primary" icon="el-icon-search">搜索</el-button>
+        
+        <div class="block">
+            <span class="demonstration">带快捷选项</span>
+            <el-date-picker
+            v-model="value2"
+            align="right"
+            type="date"
+            placeholder="选择日期"
+            :picker-options="pickerOptions">
+            </el-date-picker>
+        </div>
+    
+    </div>
+    
+</template>
+
+<script>
+export default {
+data() {
+      return {
+        pickerOptions: {
+          disabledDate(time) {
+            return time.getTime() > Date.now();
+          },
+          shortcuts: [{
+            text: '今天',
+            onClick(picker) {
+              picker.$emit('pick', new Date());
+            }
+          }, {
+            text: '昨天',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit('pick', date);
+            }
+          }, {
+            text: '一周前',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', date);
+            }
+          }]
+        },
+        value1: '',
+        value2: '',
+      };
+    }
+}
+</script>
+
+
+<style scoped>
+</style>
